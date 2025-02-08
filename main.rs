bc1c855b823ae5ebbf4bd8e812f5474d48fe352a
tphil112358 <156223399+tphil112358@users.noreply.github.com>
2025-02-07 21:51:02 -0700

    Updates from today's work

--- main.rs
+++ main.rs
@@ -1,17 +1,18 @@
 use rand::Rng;
+use std::io;
 
-const MODULUS: f64 = 9173.0;
+const MODULUS: i64 = 9173;
 
 fn generate_random_number(max: u32) -> u32 {
-    let mut rng = rand::rng();
-    rng.random_range(1..=max)
+    let mut rng = rand::thread_rng();
+    rng.gen_range(1..=max)
 }
 
 
-fn point_doubling(x1: f64, y1: f64) -> (f64, f64) {
+fn point_doubling(x1: i64, y1: i64) -> (i64, i64) {
     // Compute the slope s
-    let mut s = (3.0 * x1 * x1 + 2.0).rem_euclid(MODULUS);
-    let t = mod_inverse(y1*2.0, MODULUS);
+    let mut s = (3 * x1 * x1 + 2).rem_euclid(MODULUS);
+    let t = mod_inverse(y1*2, MODULUS);
     s = s * t;
     
     s = s.rem_euclid(MODULUS);
@@ -28,7 +29,7 @@ fn point_doubling(x1: f64, y1: f64) -> (f64, f64) {
     (x3, y3)
 }
 
-fn point_addition(x1: f64, y1: f64, x2: f64, y2: f64) -> (f64, f64) {
+fn point_addition(x1: i64, y1: i64, x2: i64, y2: i64) -> (i64, i64) {
     // Compute the slope
     let mut s = (y2 - y1).rem_euclid(MODULUS);
     let t = mod_inverse((x2 - x1).rem_euclid(MODULUS), MODULUS);
@@ -36,68 +37,99 @@ fn point_addition(x1: f64, y1: f64, x2: f64, y2: f64) -> (f64, f64) {
             
     // Compute the new x-coordinate x_r
     let mut x_r = (s * s - x1 - x2).rem_euclid(MODULUS);
-    if x_r < 0.0 {
+    if x_r < 0 {
         x_r += MODULUS;
     }
 
     // Compute the new y-coordinate y_r
     let mut y_r = (s * (x1 - x_r) - y1).rem_euclid(MODULUS);
-    if y_r < 0.0 {
+    if y_r < 0 {
         y_r += MODULUS;
     }
 
-    (x_r, y_r)
+    (x_r.abs(), y_r.abs())
 }
 
-
-
-fn mod_inverse(a: f64, m: f64) -> f64 {
+fn mod_inverse(a: i64, m: i64) -> i64 {
     let a = a.rem_euclid(m);
     let (gcd, x, _) = extended_gcd(a, m);
-    if gcd != 1.0 {
-        return 1.0;
+    if gcd != 1 {
+        return 1;
     } else {
         return (x % m + m) % m;
     }
 }
 
-fn extended_gcd(a: f64, b: f64) -> (f64, f64, f64) {
-    if a == 0.0 {
-        return (b, 0.0, 1.0);
+fn extended_gcd(a: i64, b: i64) -> (i64, i64, i64) {
+    if a == 0 {
+        return (b, 0, 1);
     }
     let (gcd, x1, y1) = extended_gcd(b % a, a);
-    (gcd, y1 - (b / a).floor() * x1, x1)
+    (gcd, y1 - (b / a) * x1, x1)
 }
 
-fn cyclic_group(x1: f64, y1: f64) -> Vec<(f64, f64)> {
+fn cyclic_group(x1: i64, y1: i64) -> Vec<(i64, i64)> {
   
     let mut cyclic_group_nums = Vec::new();
-    cyclic_group_nums.push((x1, y1));
+
     let (mut x_new, mut y_new) = point_doubling(x1, y1);
-    
-
+    cyclic_group_nums.push((x1, y1));
     cyclic_group_nums.push((x_new, y_new));
-
+    let mut i = 2;
 
     while x_new != x1 && y_new != MODULUS -y1 {
+        i += 1;
         (x_new, y_new) = point_addition(x1, y1, x_new, y_new);
         cyclic_group_nums.push((x_new, y_new));
-        println!("x_new: {}, y_new: {}", x_new, y_new);
+        println!("{}G: ({}, {})", i, x_new, y_new);
+        if i % 500 == 0 {
+            println!("Press enter to continue: ");
+            io::stdin().read_line(&mut String::new());
+        }
     }
-
     cyclic_group_nums
 }
 
+fn generate_ciphertext(x: i64, y: i64) -> (i64, i64, i64, i64) {
+    let k = generate_random_number(MODULUS);
+    (1, 1, 1, 1)
+}
+
+fn char_to_point(c: char, cyclic_group: Vec<(i64, i64)>) -> (i64, i64) {
+    let unicode_value: u32 = c as u32;
+    if unicode_value > 1114111
+        {panic!("Unicode value is greater than 1114111");}
+    get_g_point(unicode_value, cyclic_group)
+}
+
+fn point_to_char(x: i64, y: i64, cyclic_group: Vec<(i64, i64)>) -> char {
+    let mut i: u32 = 1;
+    
+    for point in cyclic_group {
+        if i > 1114111 {
+            panic!("Invalid character!"); // 1114111 is the max unicode value
+        }
+        if point.0 == x && point.1 == y {
+            if let Some(c) = std::char::from_u32(i) {
+                return c; }
+        }
+        i += 1;
+    }
+    ' ' // Returns a space if no match is found; this state is intended to be unreachable
+}
+
+fn get_g_point(g: u32, cyclic_group: Vec<(i64, i64)>) -> (i64, i64) {
+    let index = g as usize;
+    cyclic_group[index]
+}
 
 fn main() {
-    let x1 = 5.0;
-    let y1 = 1.0;
-    let result = cyclic_group(x1, y1);
-    println!("Cyclic group: {:?}", result);
+    let x1 = 5;
+    let y1 = 1;
+    let whole_cyclic_group = cyclic_group(x1, y1);
+    println!("Cyclic group: {:?}", whole_cyclic_group);
 
     // get the random number
     let random_number = generate_random_number(10);
     println!("Random number: {}", random_number);
 }
-
-
